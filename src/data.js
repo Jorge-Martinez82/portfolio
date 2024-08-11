1e3952a836bd2879200d9638795f384fe221830d
cirbcibg <jmartines@gmail.com>
2024-08-11 17:17:28 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,68 @@
+import javascriptLogo from './images/javascript.png';
+import reactLogo from './images/react.png';
+import javaLogo from './images/java.png';
+import springbootLogo from './images/springboot.png';
+import sqlLogo from './images/sql.png';
+import postgresqlLogo from './images/postgresql.png';
+
+export const projects = [
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+        link: "https://reactbootcamp.com",
+    },
+    {
+        title: "React Tracks",
+        subtitle: "React and Python",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-2.gif",
+        link: "https://reedbarger.com",
+    },
+    {
+        title: "DevChat",
+        subtitle: "React and Firebase",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-3.gif",
+        link: "https://jsbootcamp.com",
+    },
+    {
+        title: "Epic Todo App",
+        subtitle: "React Hooks",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        link: "https://pythonbootcamp.com",
+    },
+];
+
+export const skills = [
+    {
+        tech: "JavaScript",
+        logo: javascriptLogo
+    },
+    {
+        tech: "React",
+        logo: reactLogo
+    },
+    {
+        tech: "Java",
+        logo: javaLogo
+    },
+    {
+        tech: "Springboot",
+        logo: springbootLogo
+    },
+    {
+        tech: "SQL",
+        logo: sqlLogo
+    },
+    {
+        tech: "PostgreSQL",
+        logo: postgresqlLogo
+    },
+];

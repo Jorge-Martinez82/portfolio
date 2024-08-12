6cc7c037250455abded56d4c97726ca69715f913
cirbcibg <jmartines@gmail.com>
2024-08-12 16:05:25 +0200

    fixing deploy

--- src/data.js
+++ src/data.js
@@ -7,36 +7,36 @@ import postgresqlLogo from './images/postgresql.png';
 
 export const projects = [
     {
-        title: "React Reserve",
+        title: "Project 1",
         subtitle: "MERN Stack",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
         image: "./project-1.gif",
-        link: "https://reactbootcamp.com",
+        link: "",
     },
     {
-        title: "React Tracks",
+        title: "Project 2",
         subtitle: "React and Python",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
         image: "./project-2.gif",
-        link: "https://reedbarger.com",
+        link: "",
     },
     {
-        title: "DevChat",
+        title: "Project 3",
         subtitle: "React and Firebase",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
         image: "./project-3.gif",
-        link: "https://jsbootcamp.com",
+        link: "",
     },
     {
-        title: "Epic Todo App",
+        title: "Project 4",
         subtitle: "React Hooks",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
         image: "./project-4.gif",
-        link: "https://pythonbootcamp.com",
+        link: "",
     },
 ];
 

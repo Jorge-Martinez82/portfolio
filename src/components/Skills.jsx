6c71c44f286c771aef0a8764af360aeb8e854ae9
cirbcibg <jmartines@gmail.com>
2024-08-13 19:47:43 +0200

    adding projects

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,7 +9,7 @@ export default function Skills() {
                 <div className="text-center mb-20">
                     <CpuChipIcon className="w-10 inline-block mb-4" />
                     <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-                        Habilidades y Tecnologías
+                        Skills &amp; Technologies
                     </h1>
                     <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
--- src/data.js
+++ src/data.js
@@ -4,22 +4,24 @@ import javaLogo from './images/java.png';
 import springbootLogo from './images/springboot.png';
 import sqlLogo from './images/sql.png';
 import postgresqlLogo from './images/postgresql.png';
+import neuroartGif from './images/neuroart.gif';
+import triviappGif from './images/triviapp.gif';
 
 export const projects = [
     {
-        title: "Project 1",
-        subtitle: "MERN Stack",
+        title: "NeuroArt",
+        subtitle: "React + Springboot + PostreSQL",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-1.gif",
-        link: "",
+            "Web application that leverages AI to generate images based on user prompts. It assists users in making their prompts more effective by offering various suggestions to enhance their initial prompts",
+        image: neuroartGif,
+        link: "https://github.com/Jorge-Martinez82/NeuroArt",
     },
     {
-        title: "Project 2",
-        subtitle: "React and Python",
+        title: "TriviAPP",
+        subtitle: "React + Springboot + PostreSQL",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-2.gif",
+            "This application is a one-day project I made for presenting in the \"Hack Day\" of Salt Bootcamp. The application uses an external API to show, save, and delete questions and aswers as a trivial-like game.",
+        image: triviappGif,
         link: "",
     },
     {

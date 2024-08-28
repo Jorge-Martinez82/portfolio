f5dd79e1730d6cb270006a2a5d9e334277cc213a
cirbcibg <jmartines@gmail.com>
2024-08-29 00:16:36 +0200

    completing text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
                     className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                         Hello, I am Jorge
-                        <br className="hidden lg:inline-block"/>A junior fullstack developer.
+                        <br className="hidden lg:inline-block"/> A junior fullstack developer.
                     </h1>
                     <p className="mb-8 leading-relaxed">
                         Seeking
